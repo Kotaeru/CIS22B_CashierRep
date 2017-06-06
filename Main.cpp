167693ee012d816de71cc8dbc9c815be29edb3b7
Kotaeru <kyle.hori64@gmail.com>
2017-06-06 14:34:30 -0700

    Add files via upload

---
+++ Main.cpp
@@ -0,0 +1,28 @@
+/*Kyle Hori (Add your name here please)
+CIS 22B
+Cash Register Program
+
+Program to simulate a book store cash register
+
+Prompt the user to select their option
+In cash register module let user input basic book information and program will bring up price info
+User confirms or denies information on screen
+If confirmed print to standard output a receipt info thing and remove book from inventory file using inventory module functions
+If denied prompt user to reenter information
+In Inventory Module user can look up, add or remove books from inventory file
+Information will be used in Book class as specified in the Project_Specifications pdf
+In the Report module will be able to look through the inventory file and find information specifed by user
+Methods of search and organizations are defined in the Project_Specifications pdf
+*/
+
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+//Main menu with options to access the modules created 
+//Don't worry about making it look nice just yet. It just needs to work right now.
+int main()
+{
+
+}
