bb8aa6ee34e6365b0ec703073fc010c7b0950e95
Kotaeru <kyle.hori64@gmail.com>
2017-06-13 15:53:16 -0700

    Add files via upload
    So these are the files for the cash register. We still need to do a couple of things to finish it up. 1) Make sure you understand these code files, how they work and how they go together. 2) Complete the input file and output file function and implement it. 3) Improve upon the existing code to  make it more efficient since we are graded on how efficient and clean our code is. 
    If anyone has something else they'd like to add to the list of things to complete please do it in a comment. And if you take out the code, make sure its on a new branch and DO NOT commit it to the master branch until at least one other person has test run it. And if you are test running it, your job is to break the code, which also means you don't necessarily have to follow the on screen prompts given. Like if it asks for an int try inputting a char and see what happens. That's about it. If you don't understand the code, please ask because if someone knows then we'll all know and if no one knows, then we know we h

---
+++ bookdata.h
@@ -0,0 +1,43 @@
+// ********************************************************
+// Starting Out with C++                                  *
+// From Control Stuctures through Objects                 *
+// seventh edition                                        *
+//                                                        *
+// Chapter 11 Structured Data                             *
+//                                                        *
+// Serendipity Booksellers Software Development           *
+// Project — Part 11: A Problem-Solving Exercise          *
+//                                                        *
+// Multi-File Program                                     *
+// ********************************************************
+#include <string.h>
+const int STR_SIZE  = 51;   // represents the maximum string size for bookTitle
+const int ISBN_WIDTH = 14;	// the ISBN display length
+const int AU_SIZE = 31;     // the maximum number of characters for author string
+const int NUM_BOOKS = 20;	// the number of books in inventory
+
+// BookData structure declaration.
+
+struct BookData
+{
+	char bookTitle[STR_SIZE],	// stores the tile of a book
+         isbn[ISBN_WIDTH],	    // stores the isbn number of a book
+         author[AU_SIZE],       // stores the book author's name
+         publisher[31],	        // stores the book publisher's name
+         dateAdded[11];	        // stores the date a book was added into inventory
+	int	qtyOnHand;		        // stores the quantity on hand of a book
+	double wholesale,	        // stores the wholesale price of a book
+           retail;		        // stores the retail price of a book
+};
+
+// Function prototypes
+void setTitle(char* , int);
+void setISBN(char* , int);
+void setAuthor(char* , int);
+void setPub(char* , int);
+void setDateAdded(char* , int);
+void setQty(int , int);
+void setWholesale(double , int);
+void setRetail(double , int);
+int isEmpty(int);
+void removeBook(int);
--- invmenu.cpp
+++ invmenu.cpp
@@ -1,3 +1,15 @@
+// ********************************************************
+// Starting Out with C++                                  *
+// From Control Stuctures through Objects                 *
+// seventh edition                                        *
+//                                                        *
+// Chapter 11 Structured Data                             *
+//                                                        *
+// Serendipity Booksellers Software Development           *
+// Project — Part 11: A Problem-Solving Exercise          *
+//                                                        *
+// Multi-File Program                                     *
+// ********************************************************
 #include <iostream>
 #include "invmenu.h"
 #include "strupper.h"
@@ -391,3 +403,172 @@ void editBook()
 	}
 
 }	// end function editBook
+//********************************************
+// deleteBook function                       *
+//********************************************
+
+void deleteBook()
+{
+	char title[STR_SIZE];	// stores a book tile the user enters
+	int  choice = 0;		// stores the user's numeric choice
+	char response = 'N';	// stores the user's alphanumeric response
+	int row;                // Used in a loop to step through the array
+
+	// prompt the user to enter a full or partial book title
+	cin.ignore();
+	cout << "\nPlease enter all or part of the title:  ";
+	cin.getline(title, STR_SIZE);
+
+	strUpper(title);		// Convert to uppercase
+
+	// enter the 'book title search' for loop
+	for (row = 0; row < NUM_BOOKS; row++)
+	{
+		// search for a match
+		if (strstr(book[row].bookTitle, title)) // if they match
+		{
+			// display a possible match
+			cout << "\nPossible Match Found:  "
+				 << book[row].bookTitle
+			     << "\n\n";
+
+			// prompt the user for confirmation
+			cout << "Is this a Correct Match?\n";
+			cout << "1.Yes\n";
+			cout << "2.No\n\n";
+
+			cout << "Enter Your Choice: ";
+			cin >> choice;
+
+			// validate the user's input
+			while (choice != 1 && choice != 2)
+			{
+				cout << "Please enter a number in the range 1 - 2\n\n";
+				cout << "Is this a Correct Match?\n";
+				cout << "1.Yes\n";
+				cout << "2.No\n\n";
+
+				cout << "Enter Your Choice: ";
+				cin >> choice;
+			}
+
+			if (choice == 1)
+			{
+				// call function 'bookInfo' to display the results
+				bookInfo(book[row].isbn, book[row].bookTitle,
+					     book[row].author, book[row].publisher,
+						 book[row].dateAdded, book[row].qtyOnHand,
+						 book[row].wholesale, book[row].retail);
+
+			// prompt the user to delete the record
+			cout << "Would you like to delete this entire record?(Y/N):  ";
+			cin >> response;
+
+			// validate the user's input
+			while( response != 'y' && response != 'Y' && response != 'n' && response != 'N')
+			{
+				cout << "\nPlease enter \'Y\' for Yes or \'N\' for No.\n\n";
+
+					cout << "Would you like to delete this entire record?(Y/N):  ";
+					cin >> response;
+			}
+
+			if (response == 'Y' || response == 'y')
+			{
+				removeBook(row);
+
+				// notify the user that the record has been deleted
+				cout << "\nRecord has been deleted.\n";
+
+			} // end if
+
+				break;
+			}
+			else
+			{
+				cout << "\nRecord was not deleted.\n";
+				continue;
+			}
+
+		}	// end if match found
+
+	}	// end the 'book title search' for loop
+
+	if (row == NUM_BOOKS)
+	{
+		cout << "\nSorry.  No title matching \'"
+			 << title
+			 << "\' was found."
+			 << "\n\n";
+	}
+
+}  // end function deleteBook
+
+//********************************************
+// invMenu function                          *
+//********************************************
+
+void invMenu()
+{
+	int choice = 0; // stores user's choice
+
+	// display the 'Inventory Database Menu' until item 5 is selected
+	while (choice != 5)
+	{
+
+		// display company name and screen title
+		cout << "\n\n\tSerendipity Booksellers\n";
+		cout << "\tInventory Database\n\n";
+
+		// display menu items
+		cout << "\t\t1.Look Up a Book\n";
+		cout << "\t\t2.Add a Book\n";
+		cout << "\t\t3.Edit a Book's Record\n";
+		cout << "\t\t4.Delete a Book\n";
+		cout << "\t\t5.Return to the Main Menu\n\n";
+
+		// display user prompt
+		cout << "\t\tEnter Your Choice: ";
+		cin >> choice;
+
+		// validate the user's input
+		while (choice < 1 || choice > 5)
+		{
+			cout << "\n\t\tPlease enter a number in the range 1 - 5.\n\n";
+
+			cout << "\t\tEnter Your Choice: ";
+			cin >> choice;
+		}
+
+		// call the selected function
+		switch (choice)
+		{
+			case 1:
+				lookUpBook();
+					break;
+
+			case 2:
+				addBook();
+					break;
+
+			case 3:
+				editBook();
+					break;
+
+			case 4:
+				deleteBook();
+					break;
+
+			case 5:
+				cout << "\n\t\tYou selected item 5.\n";
+					break;
+
+		}	// end switch
+
+	}	// end the 'Inventory Database Menu' while loop
+
+	cout << endl << endl;
+
+}	// end function invMenu
+
+
